40a289d0ef32fa2297bc838780e866dff14e51d2
oskar <osau24@student.bth.se>
2025-05-07 11:46:00 +0200

    Application code exercise - Flash messages

--- src/Controller/DiceGameController.php
+++ src/Controller/DiceGameController.php
@@ -80,6 +80,10 @@ class DiceGameController extends AbstractController
             if ($value === 1) {
                 $round = 0;
                 $roundTotal = 0;
+                $this->addFlash(
+                    'warning',
+                    'You got a 1 and you lost the round points!'
+                );
                 break;
             }
             $round += $value;
@@ -100,6 +104,11 @@ class DiceGameController extends AbstractController
 
         $session->set("pig_round", 0);
         $session->set("pig_total", $roundTotal + $gameTotal);
+
+        $this->addFlash(
+            'notice',
+            'Your round was saved to the total!'
+        );
 
         return $this->redirectToRoute('pig_play');
     }

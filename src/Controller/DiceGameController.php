ad7bf5d07e007627fd92dc38cc80c94b4c006c55
oskar <osau24@student.bth.se>
2025-05-07 10:51:05 +0200

    Application code exercise - Extend a base class

--- src/Controller/DiceGameController.php
+++ src/Controller/DiceGameController.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 use App\Dice\Dice;
+use App\Dice\DiceGraphic;
 
 class DiceGameController extends AbstractController
 {
@@ -37,7 +38,8 @@ class DiceGameController extends AbstractController
 
         $diceRoll = [];
         for ($i = 1; $i <= $num; $i++) {
-            $die = new Dice();
+            // $die = new Dice();
+            $die = new DiceGraphic();
             $die->roll();
             $diceRoll[] = $die->getAsString();
         }

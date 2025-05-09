31e4043ce028d387fa2aa822b3b535fb8db49340
oskar <osau24@student.bth.se>
2025-05-09 06:55:27 +0200

    kmom02 CardHand class to hold multiple cards

---
+++ src/Card/CardHand.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Card;
+
+use App\Card\CardGraphic;
+
+class CardHand
+{
+    private $hand = [];
+
+    public function add(CardGraphic $card): void
+    {
+        $this->hand[] = $card;
+    }
+
+    public function __toString(): string
+    {
+        $out_string = "";
+                
+        foreach ($this->hand as $card) {
+            $out_string = $out_string . $card;
+        }
+
+        return $out_string;
+    }
+
+}

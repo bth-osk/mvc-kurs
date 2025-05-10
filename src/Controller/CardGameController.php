6ab09cee76027682f4edf7979ccc8055ff1af47a
oskar <osau24@student.bth.se>
2025-05-10 06:52:29 +0200

    Removed unused Card and CardGraphics imports from CardGameController

--- src/Controller/CardGameController.php
+++ src/Controller/CardGameController.php
@@ -7,8 +7,6 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Card\Card;
-use App\Card\CardGraphic;
 use App\Card\DeckOfCards;
 use App\Card\CardHand;
 
